50e817e41d26c8797e8f03d87546adaacf19feee
Stickman375 <75806815+Stickman375@users.noreply.github.com>
2021-07-07 20:39:12 -1000

    Add files via upload

---
+++ PRO-C43-Template-main/PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "TOWEL";
+const accessCode2 = "ENVELOPE";
+const accessCode3 = "789";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("O T L W E", 100,50)
+    fill("lightblue")
+    text("Hint: Gets wet when drying", 100,70)
+// add code for displaying rest of the names and the hints.
+
+fill("white")
+    textSize(15)
+    text("V N E E O L E P", 700,150)
+    fill("lightblue")
+    text("Hint: Stats with an e ends with an e and has only 1 letter", 626,170)
+
+    fill("white")
+    textSize(15)
+    text("It is numbers not text", 250,210)
+    fill("lightblue")
+    text("Hint: Why was six scared of seven", 250,230)
+    
+
+    // 40 and 20
+}
